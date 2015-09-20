03b18cbd5d04b7edf54342bc022ee299e89dc53d
darrengtan <darrengtan@gmail.com>
2015-09-19 20:14:52 -0700

    parse dates to unix

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,16 @@
 class SubmissionsController < ApplicationController
-  def grab_submissions
+  def new
+  end
 
+  def grab_data
+    @hashtag = submission_params[:hashtag]
+    @start = Time.parse(submission_params[:start]).to_i
+    @end = Time.parse(submission_params[:end]).to_i
+    fail
+  end
+
+  private
+  def submission_params
+    params.require(:submission).permit(:hashtag, :start, :end)
   end
 end
