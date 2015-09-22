61c6b3655ff229401015c917ae4200dddf5c8255
darrengtan <darrengtan@gmail.com>
2015-09-22 10:15:50 -0700

    start specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  it "renders root template" do
+    get :root
+    expect(response).to render_template :root
+  end
 end
--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SubmissionsController, type: :controller do
+RSpec.describe Api::SubmissionsController, type: :controller do
 
 end
