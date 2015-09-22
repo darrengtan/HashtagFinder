cc2776779f83e620a4c949af301b884ba3871364
darrengtan <darrengtan@gmail.com>
2015-09-22 12:43:57 -0700

    write submission model spec

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :submission do
-    
+    hashtag "snow"
+    start_date "2015-09-17"
+    end_date "2015-09-18"
+    response HTTParty.get("https://api.instagram.com/v1/tags/snow/media/recent?client_id=#{ENV['CLIENT_ID']}&max_tag_id=1442473200&min_tag_id=1442559600").body
   end
 
+  factory :bad_submission, class: Submission do
+    hashtag "snow"
+    start_date "2015-09-18"
+    end_date "2015-09-17"
+    response HTTParty.get("https://api.instagram.com/v1/tags/snow/media/recent?client_id=#{ENV['CLIENT_ID']}&max_tag_id=1442559600&min_tag_id=1442473200").body
+  end
 end
--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Submission, type: :model do
+  let(:submission) { FactoryGirl.build(:submission) }
+  let(:bad_submission) { FactoryGirl.build(:bad_submission) }
+
+  describe "validation" do
+    it "validates good submissions" do
+      expect(submission).to be_valid
+    end
+
+    it "validates end date after start date" do
+      expect(bad_submission).to be_invalid
+    end
+  end
+
+  describe "Submission::methods" do
+    it "gets appropriate data from request" do
+      response = Submission.get_data("snow", "2015-09-17", "2015-09-18")
+      expect(response.body).to include("pagination")
+    end
+
+    it "gets next page" do
+      response = Submission.get_next_page(submission.next_page).body
+      expect(response).to include("pagination")
+      expect(response).to_not eq(submission.response)
+    end
+  end
+
+  describe "Submission#methods" do
+    it "returns next page url" do
+      expect(submission.next_page).to include(submission.hashtag)
+    end
+
+    it "returns instagram data" do
+      expect(submission.collection_data.length).to eq(20)
+    end
+
+  end
 end
